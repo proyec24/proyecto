1da212699fac88d129ea04061b0480817a8d33a6
Alvaro <alvariux2008@gmail.com>
2020-06-29 01:15:45 -0500

    version final

---
+++ delete.php
@@ -0,0 +1,13 @@
+<?php
+    include 'db.php';
+
+	$usua = filter_input(INPUT_POST, "id");
+
+  	$query = "DELETE FROM trabajadores WHERE id='$usua'";
+  	$result = mysqli_query($conn, $query);
+  	$row = mysqli_fetch_array($result);
+  	if($row){
+    	echo '1';
+  	}
+  	mysqli_close($conn);
+?>
---
+++ modify.php
@@ -0,0 +1,16 @@
+<?php
+    include 'db.php';
+    
+    $name = filter_input(INPUT_POST, "name");
+    $id = filter_input(INPUT_POST, "id");
+    $lname = filter_input(INPUT_POST, "lname");
+    $age = filter_input(INPUT_POST, "age");
+    $phone = filter_input(INPUT_POST, "phone");
+  	$query = "UPDATE trabajadores SET nombre='$name',apellidos='$lname',edad='$age', telefono='$phone' WHERE id='$id' ";
+  	$result = mysqli_query($conn, $query);
+  	$row = mysqli_fetch_array($result);
+  	if($row){
+    	echo '1';
+  	}
+  	mysqli_close($conn);
+?>
