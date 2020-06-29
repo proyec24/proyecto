5b0235375926a1096a7d4b9eecc0da71f9e80e0c
Alvaro <alvariux2008@gmail.com>
2020-06-28 21:20:52 -0500

    segunda version

--- db.php
+++ db.php
@@ -1,10 +1,12 @@
 <?php
-	$user="root";
-	$pass="";
-	$server="localhost";
-	$db="db_proyecto";
+	$user="bb9748f13e1e28";
+	$pass="40a5c5ce";
+	$server="us-cdbr-east-02.cleardb.com";
+	$db="heroku_1e307505c181aea";
 	$conn = new mysqli($server, $user, $pass, $db);
  	if($conn->connect_error){
     	die("Error de conexion: " .$conn->connection_error);
+  	}else{
+  		echo"1";
   	}
 ?>
